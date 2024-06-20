e364b1d35c54088e5c637843f27c89ba292c14b9
dpDifficult <126591369+dpDifficult@users.noreply.github.com>
2024-06-20 17:23:12 +0800

    Add files via upload

---
+++ ParsePrefab.cs
@@ -0,0 +1,191 @@
+﻿using UnityEditor;
+using UnityEngine;
+using System.Collections.Generic;
+using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace ET
+{
+    public class ParsePrefab : EditorWindow
+    {
+#if UNITY_EDITOR
+        [MenuItem("ParsePrefab/ParsePrefab")] // 创建一个菜单项，在Unity菜单栏中显示
+        public static void ShowWindow()
+        {
+            EditorWindow.GetWindow(typeof(ParsePrefab)); // 创建并显示自定义Editor窗口
+        }
+
+        void OnGUI()
+        {
+            GUILayout.Label("拖入___Window.Prefab", EditorStyles.boldLabel);
+
+            Event evt = Event.current;
+            Rect dropArea = GUILayoutUtility.GetRect(0.0f, 50.0f, GUILayout.ExpandWidth(true));
+            GUI.Box(dropArea, "Drop files here");
+            switch (evt.type)
+            {
+                case EventType.DragUpdated:
+                case EventType.DragPerform:
+                    if (!dropArea.Contains(evt.mousePosition))
+                        break;
+
+                    DragAndDrop.visualMode = DragAndDropVisualMode.Copy;
+
+                    if (evt.type == EventType.DragPerform)
+                    {
+                        DragAndDrop.AcceptDrag();
+
+                        foreach (var draggedObject in DragAndDrop.objectReferences)
+                        {
+                            this.path = AssetDatabase.GetAssetPath(draggedObject);
+                            if (!string.IsNullOrEmpty(path) && File.Exists(path))
+                            {
+                                break;
+                            }
+                        }
+                    }
+                    Event.current.Use();
+                    break;
+            }
+            
+            GUILayout.Label("File Path: " + this.path, EditorStyles.wordWrappedLabel);
+
+            if (GUILayout.Button("Load Window Prefab and Extract Data"))
+            {
+                GetReferenceCollector();
+            }
+            
+            
+            GUILayout.Space(20);
+            GUILayout.BeginHorizontal();
+            GUILayout.Space(20);
+            
+            scrollPosition = EditorGUILayout.BeginScrollView(scrollPosition, GUILayout.Height(500)); // 开始滚动视图
+            input = EditorGUILayout.TextArea(input, GUILayout.ExpandHeight(true)); // 文本区域，允许高度扩展
+            EditorGUILayout.EndScrollView(); // 结束滚动视图
+            GUILayout.EndHorizontal();
+            
+        }
+        
+
+        private string path;
+        public List<string> Data = new List<string>(64);
+        public string input;
+        
+
+        private Vector2 scrollPosition = Vector2.zero;
+
+        private void GetReferenceCollector()
+        {
+            
+            input = "";
+
+            var prefab = AssetDatabase.LoadAssetAtPath<GameObject>(this.path);
+            if (prefab == null)
+            {
+                Debug.LogError("prefab is null , check the file path");
+                return;
+            }
+            
+            var rc = prefab.GetComponent<ReferenceCollector>();
+
+            ReferenceCollectorParser(prefab.name, rc, 0);
+        }
+
+        private void ReferenceCollectorParser(string titleName, ReferenceCollector rc,int depth)
+        {
+            
+            SerializedObject serializedObject = new SerializedObject(rc);
+            SerializedProperty dataProperty = serializedObject.FindProperty("data");
+            
+            for (int i = 0; i < dataProperty.arraySize; ++i)
+            {
+                SerializedProperty element = dataProperty.GetArrayElementAtIndex(i);
+                var obj = element.FindPropertyRelative("gameObject").objectReferenceValue;
+
+                if (obj is GameObject || obj is Component) //先判断是不是gameObject和组件
+                {
+                    var go = obj as GameObject;
+                    if (!go)
+                    {
+                        go = ((Component)obj).gameObject;
+                    }
+                    
+                    {
+                        List<Component> componentList = new List<Component>(4);
+                        go.GetComponents(componentList); //将gameObject身上所有的组件存放到list里
+                        foreach (var component in componentList)
+                        {
+                            if (component is ReferenceCollector && go.name.Contains("Form")) 
+                            {
+                                if (depth > 0)
+                                {
+                                    continue;
+                                }
+                                ReferenceCollectorParser(go.name,component as ReferenceCollector, 1);
+                                
+                            }
+                        }
+                        
+                    }
+                    
+                }
+
+                if (obj == null)
+                {
+                    Debug.LogWarning("reference collector组件右侧有空对象!");
+                }
+                else
+                {
+                    string name = obj.name;
+                    string typeStr = obj.GetType().ToString();
+                    int lastIndex = typeStr.LastIndexOf('.');
+
+                    if (lastIndex != -1 && lastIndex < typeStr.Length - 1)
+                    {
+                        typeStr = typeStr.Substring(lastIndex + 1); // 提取点号后的子字符串
+                    }
+
+                    Data.Add(typeStr);
+                    Data.Add(name);
+                }
+                
+            }
+
+            if (titleName.Contains("Window"))
+            {
+                return;
+            }
+            
+            input += "******" + titleName + "******" + "\n";
+
+            StringBuilder stringBuilder = new StringBuilder();
+            for (int i = 0; i < Data.Count; i += 2)
+            { 
+                stringBuilder.Append("public " + Data[i] + " " + Data[i + 1] + "\n");
+            }
+
+            stringBuilder.Append("\n");
+
+            stringBuilder.Append("************" + titleName + "System :" + "************" + "\n");
+
+            for (int i = 0; i < Data.Count; i += 2)
+            {
+                stringBuilder.Append("self." + Data[i + 1] + " = " + "self.Collector.Get<" + Data[i] + ">(" + "\"" + Data[i + 1] + "\")");
+                stringBuilder.Append("\n");
+            }
+
+            input += stringBuilder.ToString();
+            input += "\n\n";
+            
+            Data.Clear();
+        }
+
+
+
+#endif
+
+    }
+}
